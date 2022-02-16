452aa02b576420156c1df8e7a9217ed914cf9ce8
ShaileshKV29 <shailesh29062001@gmail.com>
2022-02-16 14:43:13 +0530

    NumPy Matrix

---
+++ NumPy/numpy_matrix.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+b = np.array([
+    [1,2,3],
+    [4,5,6]
+])
+
+c = np.array([
+    [10, 11],
+    [20, 21],
+    [30, 31]
+])
+
+print(b)
+print(c)
+print(b.ndim)
+print(b.shape)
